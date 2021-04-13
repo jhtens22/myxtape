2430e087cdef9e46db7f5d39caa34e2df5c75dcc
jhtens22 <jaythanhew@mail.weber.edu>
2021-04-13 16:57:40 -0600

    header

---
+++ header.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>My Myxtape</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="https://getbootstrap.com/docs/4.0/examples/sticky-footer-navbar/sticky-footer-navbar.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <h1 class="text-center text-primary">
+    My Myxtape
+    </h1>
