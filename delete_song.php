653bd51e131807e4f0def92567c1cf0c9c035405
jhtens22 <jaythanhew@mail.weber.edu>
2021-04-13 19:45:40 -0600

    maybe

--- delete_song.php
+++ delete_song.php
@@ -1,12 +1,12 @@
 <?php
     echo $_GET["song_id"];
-    echo $_GET[$_SESSION["user_id"]];
 
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 
     require_once('./song/song.php');
+    require_once('./sessioncheck.php');
 
     $song = new song();
     $songs = $song->deleteSong($_GET[$_SESSION["user_id"]], $_GET["song_id"]);
