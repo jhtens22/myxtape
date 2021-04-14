bf2dde5e18ecf015ab9398c16a2ed50866776c4b
jhtens22 <jaythanhew@mail.weber.edu>
2021-04-13 19:55:22 -0600

    sessioncheck

--- delete_song.php
+++ delete_song.php
@@ -4,10 +4,9 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-
+    require_once('./sessioncheck.php');
     require_once('./song/song.php');
-    require_once('./session/session.php');
-
+    
     $song = new song();
     $songs = $song->deleteSong($_SESSION["user_id"], $_GET["song_id"]);
 
