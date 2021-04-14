af404f39fd39eeb29d38363f49a2151a8c86c831
jhtens22 <jaythanhew@mail.weber.edu>
2021-04-13 20:00:29 -0600

    i

--- dashboard.php
+++ dashboard.php
@@ -15,7 +15,6 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-    require_once("./sessioncheck.php");
     require_once('./song/song.php');
 
     $song = new song();
