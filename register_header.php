70a46f27e73cd116ce831f478bbcb2c2b94fb995
jhtens22 <jaythanhew@mail.weber.edu>
2021-04-25 16:29:28 -0600

    register nav

--- register_header.php
+++ register_header.php
@@ -25,3 +25,23 @@ error_reporting(E_ALL);
   </head>
 
   <body>
+    <header>
+      <nav class="navbar navbar-expand-lg">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="index.html">
+                <img src="img/myxtape_logo.png" alt="myxtape" width="50px" height="30px">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+                <a class="nav-link active" href="index.html">Home</a>
+                <a class="nav-link" href="dashboard.php">Playlist</a>
+                <a class="nav-link" href="login.html">Login</a>
+                <a class="nav-link" href="register_user.php">Create Account</a>
+              </div>
+            </div>
+          </div>
+      </nav>
+    </header>
