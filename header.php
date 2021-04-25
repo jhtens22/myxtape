456df90e9d3c148c6e85d79f8ed24a61ed082c51
jhtens22 <jaythanhew@mail.weber.edu>
2021-04-25 15:34:29 -0600

    nav

--- header.php
+++ header.php
@@ -32,6 +32,27 @@ require_once('./user/user.php')
       $thisUser = $user->getUser($_SESSION["user_id"]);
     ?>
 
+    <header>
+      <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="index.html">
+            <img src="img/myxtape_logo.png" alt="myxtape" width="50px" height="30px">
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link active" href="index.html">Home</a>
+              <a class="nav-link" href="dashboard.php">Playlist</a>
+              <a class="nav-link" href="login.html">Login</a>
+              <a class="nav-link" href="register_user.php">Create Account</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+
     <h1 class="text-center text-primary">
       <?php echo $thisUser->getUsername(); ?>'s Myxtape
     </h1>
